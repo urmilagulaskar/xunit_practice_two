ea76986c7fbda33ccd14f542ced7a4ecf41bf9fe
Urmila Gulaskar <urmila_gulaskar@persistent.co.in>
2020-07-31 15:02:03 +0530

    checking versioning

--- TimeManagementBooking/Employee.cs
+++ TimeManagementBooking/Employee.cs
@@ -6,7 +6,6 @@ namespace TimeManagementBooking
     {
         public int empId { get; set; }  
         public string empName { get; set; } 
-        public int testid {get; set;}
-
+       
     }
 }
