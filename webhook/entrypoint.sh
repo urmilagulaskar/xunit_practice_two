1311005c8c5207c331a901e486cf88b2f8e8dec5
Urmila Gulaskar <69185900+ugulaskarBT@users.noreply.github.com>
2020-08-20 19:48:13 +0530

    test webhook

---
+++ webhook/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export webhook=$1
+export message=${@:2}
+
+cat << EOF > webhook.py
+#!/usr/bin/python3
+import pymsteams
+import sys
+webhook = sys.argv[1]
+message = ' '.join(sys.argv[2:])
+myTeamsMessage = pymsteams.connectorcard(webhook)
+myTeamsMessage.text(message)
+myTeamsMessage.send()
+EOF
+
+chmod 755 webhook.py
+
+./webhook.py $webhook $message
